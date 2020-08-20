1e44ff0c035f7024d316647a445e3d37e37d8133
Alan Tan <alanwuha91@gmail.com>
2020-08-20 11:37:46 +0800

    Add CustomerApplication

---
+++ application/src/main/java/com/example/demo/customer/CustomerApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo.customer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CustomerApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+		logger.info("CustomerApplication Started........");
+	}
+}

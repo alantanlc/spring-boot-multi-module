1ad39b4ac04d4712b4c0812ae7857c2feefbf65b
Alan Tan <alanwuha91@gmail.com>
2020-08-24 18:16:57 +0800

    Enable toggling of banner of SpringApplication

--- application/src/main/java/com/example/demo/customer/CustomerApplication.java
+++ application/src/main/java/com/example/demo/customer/CustomerApplication.java
@@ -2,6 +2,7 @@ package com.example.demo.customer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerApplication {
 	private static final Logger logger = LoggerFactory.getLogger(CustomerApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
-		logger.info("CustomerApplication Started........");
+//		SpringApplication.run(CustomerApplication.class, args);
+//		logger.info("CustomerApplication Started........");
+		
+		SpringApplication app = new SpringApplication(CustomerApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 }
